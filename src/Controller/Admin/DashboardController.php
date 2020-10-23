6acf1b723eae973969de703a3feb9a9d1c1eaa66
Axel Gromat <axel.gromat@gmail.com>
2020-10-23 11:05:57 +0200

    correcting initial

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -15,7 +15,9 @@ class DashboardController extends AbstractDashboardController
      */
     public function index(): Response
     {
-        return parent::index();
+        return $this->render('bundles/EasyAdminBundle/welcome.html.twig', [
+            'user' => [],
+        ]);
     }
 
     public function configureDashboard(): Dashboard
